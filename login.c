1ac9b43d8d6c90d3f9bfd811cd48430ba6584e12
Ali Hussain <ali.hussaino71205o@gmail.com>
2025-11-17 23:24:57 +0500

    Added encrypt and decrypt in login.h

--- login.c
+++ login.c
@@ -2,54 +2,94 @@
 #include <string.h>
 #include <stdlib.h>
 #include "login.h"
-#define FILEPATH "users.txt"
 
+#define FILEPATH "users.txt"
+#define XOR_KEY 7 
+
+// Encrypt using XOR
+int encrypt(char *dest, const char *src) {
+    int i = 0;
+    while (src[i] != '\0') {
+        dest[i] = src[i] ^ XOR_KEY;
+        i++;
+    }
+    dest[i] = '\0';
+    return 0;
+}
+
+
+int decrypt(char *dest, const char *src) {
+    int i = 0;
+    while (src[i] != '\0') {
+        dest[i] = src[i] ^ XOR_KEY;
+        i++;
+    }
+    dest[i] = '\0';
+    return 0;
+}
+
+// Signup (register)
 int signup(const char *username, const char *password) {
 
-    FILE *fp = fopen(FILEPATH, "a+");  
+    FILE *fp = fopen(FILEPATH, "a+");
     if (!fp) {
-        return LOGIN_FILE_ERROR; 
+        return LOGIN_FILE_ERROR;
     }
 
     char fileUser[100], filePass[100];
 
-    // Go to start to read existing data
+    // Move to beginning to read
     rewind(fp);
 
-    // Check if username already exists
+    // Check for duplicate username
     while (fscanf(fp, "%s %s", fileUser, filePass) == 2) {
         if (strcmp(fileUser, username) == 0) {
             fclose(fp);
-            return LOGIN_USERNAME_EXISTS; 
+            return LOGIN_USERNAME_EXISTS;
         }
     }
 
-    // Append new username password at end
-    fprintf(fp, "%s %s\n", username, password);
+    // Encrypt password before storing
+    char encPass[100];
+    encrypt(encPass, password);
+
+    fprintf(fp, "%s %s\n", username, encPass);
 
     fclose(fp);
     return LOGIN_OK;
 }
 
+// Login (check credentials)
 int login(const char *username, const char *password) {
-    FILE *fp = fopen(FILEPATH, "r");   // read only
-    if (!fp) return LOGIN_FILE_ERROR;
+
+    FILE *fp = fopen(FILEPATH, "r");
+    if (!fp) {
+        return LOGIN_FILE_ERROR;
+    }
 
     char line[256], fileUser[100], filePass[100];
     int found = 0;
 
     while (fgets(line, sizeof(line), fp)) {
         if (sscanf(line, "%s %s", fileUser, filePass) == 2) {
-            if (strcmp(fileUser, username) == 0 &&
-                strcmp(filePass, password) == 0) {
-                found = 1;
-                break;
+            
+            if (strcmp(fileUser, username) == 0) {
+
+                char decPass[100];
+                decrypt(decPass, filePass);
+
+                if (strcmp(decPass, password) == 0) {
+                    found = 1;
+                    break;
+                }
             }
         }
     }
 
     fclose(fp);
 
-    if (found) return LOGIN_OK;
-    else return LOGIN_INVALID_CREDENTIALS;
+    if (found)
+        return LOGIN_OK;
+    else
+        return LOGIN_INVALID_CREDENTIALS;
 }
--- login.h
+++ login.h
@@ -1,6 +1,5 @@
 
 #define LOGIN_H
-
 #define LOGIN_OK 0
 #define LOGIN_USERNAME_EXISTS 1
 #define LOGIN_FILE_ERROR 2
@@ -8,4 +7,6 @@
 
 int signup(const char *username, const char *password);
 int login(const char *username, const char *password);
+int encrypt(char *dest, const char *src);
+int decrypt(char *dest, const char *src);
 
